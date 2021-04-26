b06e904b2c95fb39a6de0cdb1ef9db52069863ff
ahmadsiddiquech <muhammad.ahmad@xpertspot.com>
2021-04-26 17:16:08 +0500

    first day

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,45 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router'
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MatCarouselModule } from '@ngmodule/material-carousel';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FlyersComponent } from './flyers/flyers.component';
+import { HomeComponent } from './home/home.component';
+import { AccountComponent } from './account/account.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FlyersComponent,
+    FooterComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgbModule,
+    MatCarouselModule.forRoot(),
+    RouterModule.forRoot([
+      { path: '',component: HomeComponent },
+      { path: 'flyers',component: FlyersComponent },
+      { path: 'login',component: LoginComponent },
+      { path: 'register',component: RegisterComponent },
+      { path: 'account',component: AccountComponent }
+    ]),
+    
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/flyers/flyers.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'flyers',
+  templateUrl: './flyers.component.html',
+  styleUrls: ['./flyers.component.css']
+})
+export class FlyersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    
+  }
+
+}
