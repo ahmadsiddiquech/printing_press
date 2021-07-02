b83fae54d7096efd0b55aa9fa31f53bee5fe186b
Ahmad Siddique <ahmadsiddiquech@gmail.com>
2021-07-02 21:41:13 +0500

    product front

--- src/app/all-products/all-products.component.ts
+++ src/app/all-products/all-products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../common/services/products.service';
 
 @Component({
   selector: 'all-products',
@@ -7,5 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AllProductsComponent {
 
+  products: any;
+  result: any;
+  data: any;
 
+  constructor(private productService: ProductsService) { }
+
+  ngOnInit() {
+    this.productService.getProductSubject.subscribe(() => {
+      this.fetchProducts();
+    })
+
+  }
+
+  fetchProducts() {
+    this.productService.getProducts()
+      .subscribe(
+        response => {
+          this.result = response;
+          if (this.result.success) {
+            this.products = this.result.data;
+          }
+        },
+        error => {
+          console.log(error)
+        }
+      )
+  }
 }
