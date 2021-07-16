53a06bef8bd1770808ab67ef160b7aa553c9640a
Ahmad Siddique <ahmadsiddiquech@gmail.com>
2021-07-16 20:15:36 +0500

    changes in product page

--- src/app/flyer-printing/flyer-printing.component.ts
+++ src/app/flyer-printing/flyer-printing.component.ts
@@ -1,14 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ProductsService } from '../common/services/products.service';
 
 @Component({
   selector: 'app-flyer-printing',
   templateUrl: './flyer-printing.component.html',
-  styleUrls: ['./flyer-printing.component.css']
+  styleUrls: ['./flyer-printing.component.css'],
+  providers: [MatSnackBar]
 })
 
 
-export class FlyerPrintingComponent{
+export class FlyerPrintingComponent {
+  result: any;
+  products: any;
+  constructor(private productService: ProductsService, private snackBar: MatSnackBar) { }
 
-  
+  ngOnInit() {
+    this.productService.getProductSubject.subscribe(() => {
+      this.fetchProducts();
+    })
+
+  }
+
+  fetchProducts() {
+    this.productService.getProducts()
+      .subscribe(
+        response => {
+          this.result = response;
+          if (this.result.success) {
+            this.products = this.result.data;
+          }
+        },
+        error => {
+          console.log(error)
+        }
+      )
+  }
 
 }
