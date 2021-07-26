f9f3c726a68c96de637b4c5f42b02b2fb6b3c060
Ahmad Siddique <ahmadsiddiquech@gmail.com>
2021-07-26 20:01:55 +0500

    product options changes

--- src/app/common/services/products.service.ts
+++ src/app/common/services/products.service.ts
@@ -19,14 +19,6 @@ export class ProductsService {
     return this.http.get(this.productsUrl + '/' + id);
   }
 
-  getProductFinishingSize(id: any) {
-    return this.http.get(this.productsUrl + '/product_finishing_size/' + id);
-  }
-
-  getProductPrintedPages(f_size: any, id: any) {
-    return this.http.get(this.productsUrl + '/product_printed_pages/' + id);
-  }
-
   addProduct(product: any) {
     return this.http.post(this.productsUrl, product);
   }
@@ -41,5 +33,59 @@ export class ProductsService {
 
   updateProductsImage(id: any, product: any) {
     return this.http.put(this.productsUrl + '/upload_image/' + id, product);
+  }
+
+  getProductFinishingSize(id: any) {
+    return this.http.get(this.productsUrl + '/product_finishing_size/' + id);
+  }
+
+  getProductPrintedPages(f_size: any, id: any) {
+    var data = {
+      "id": id,
+      "f_size": f_size
+    };
+    return this.http.post(this.productsUrl + '/product_printed_pages/', data);
+  }
+
+  getProductStocks(p_page: any, f_size: any, id: any) {
+    var data = {
+      "id": id,
+      "p_page": p_page,
+      "f_size": f_size
+    };
+    return this.http.post(this.productsUrl + '/product_stocks/', data);
+  }
+
+  getProductCovers(p_stock: any, p_page: any, f_size: any, id: any) {
+    var data = {
+      "id": id,
+      "p_stock": p_stock,
+      "p_page": p_page,
+      "f_size": f_size
+    };
+    return this.http.post(this.productsUrl + '/product_covers/', data);
+  }
+
+  getProductLaminations(p_cover: any, p_stock: any, p_page: any, f_size: any, id: any) {
+    var data = {
+      "id": id,
+      "p_cover": p_cover,
+      "p_stock": p_stock,
+      "p_page": p_page,
+      "f_size": f_size
+    };
+    return this.http.post(this.productsUrl + '/product_laminations/', data);
+  }
+
+  getProductprices(p_lamination: any, p_cover: any, p_stock: any, p_page: any, f_size: any, id: any) {
+    var data = {
+      "id": id,
+      "p_lamination": p_lamination,
+      "p_cover": p_cover,
+      "p_stock": p_stock,
+      "p_page": p_page,
+      "f_size": f_size
+    };
+    return this.http.post(this.productsUrl + '/product_prices/', data);
   }
 }
--- src/app/flyer-printing/flyer-printing.component.ts
+++ src/app/flyer-printing/flyer-printing.component.ts
@@ -17,8 +17,12 @@ export class FlyerPrintingComponent {
   id: any;
   result: any;
   product: any;
-  finishing_size: any;
   printed_pages: any;
+  finishing_sizes: any;
+  stocks: any;
+  covers: any;
+  laminations: any;
+  prices: any;
   constructor(private productService: ProductsService, private snackBar: MatSnackBar, private router: ActivatedRoute) { }
 
   ngOnInit() {
@@ -30,31 +34,15 @@ export class FlyerPrintingComponent {
   }
 
   product_form = new FormGroup({
-    name: new FormControl(''),
-    // price: new FormControl(''),
-    subcategory_id: new FormControl(''),
-    category_id: new FormControl(''),
-    // finishingoptions_id: new FormControl(''),
-    description: new FormControl(''),
-    image: new FormControl(''),
+    finishing_size: new FormControl(''),
+    printed_page: new FormControl(''),
+    stock: new FormControl(''),
+    cover: new FormControl(''),
+    lamination: new FormControl(''),
   });
 
   submitProduct() {
 
-  }
-
-  change_finishing_size(f_size: any) {
-    this.productService.getProductPrintedPages(f_size, this.id)
-      .subscribe(
-        response => {
-          this.result = response;
-          if (this.result.success) {
-            this.printed_pages = this.result.data;
-          } else {
-            this.printed_pages = [{ id: 0, name: 'No Record Found' }];
-          }
-        }
-      )
   }
 
   fetchProduct() {
@@ -76,8 +64,7 @@ export class FlyerPrintingComponent {
           this.result = response;
           console.log(this.result);
           if (this.result.success) {
-            this.finishing_size = this.result.data;
-
+            this.finishing_sizes = this.result.data;
           }
         },
         error => {
@@ -85,4 +72,92 @@ export class FlyerPrintingComponent {
         }
       )
   }
+
+  change_finishing_size(f_size: any) {
+    this.product_form.controls['printed_page'].reset();
+    this.product_form.controls['stock'].reset();
+    this.product_form.controls['cover'].reset();
+    this.product_form.controls['lamination'].reset();
+    this.prices = '';
+    this.productService.getProductPrintedPages(f_size, this.id)
+      .subscribe(
+        response => {
+          this.result = response;
+          if (this.result.success) {
+            this.printed_pages = this.result.data;
+
+          } else {
+            this.printed_pages = [{ id: 0, name: 'No Record Found' }];
+          }
+        }
+      )
+  }
+
+  change_printed_page(p_page: any, f_size: any) {
+    this.product_form.controls['stock'].reset();
+    this.product_form.controls['cover'].reset();
+    this.product_form.controls['lamination'].reset();
+    this.prices = '';
+    this.productService.getProductStocks(p_page, f_size, this.id)
+      .subscribe(
+        response => {
+          this.result = response;
+          if (this.result.success) {
+            this.stocks = this.result.data;
+          } else {
+            this.stocks = [{ id: 0, name: 'No Record Found' }];
+          }
+        }
+      )
+  }
+
+  change_stock(p_stock: any, p_page: any, f_size: any) {
+    this.product_form.controls['cover'].reset();
+    this.product_form.controls['lamination'].reset();
+    this.prices = '';
+    this.productService.getProductCovers(p_stock, p_page, f_size, this.id)
+      .subscribe(
+        response => {
+          this.result = response;
+          if (this.result.success) {
+            this.covers = this.result.data;
+          } else {
+            this.covers = [{ id: 0, name: 'No Record Found' }];
+          }
+        }
+      )
+  }
+
+  change_cover(p_cover: any, p_stock: any, p_page: any, f_size: any) {
+    this.product_form.controls['lamination'].reset();
+    this.prices = '';
+    this.productService.getProductLaminations(p_cover, p_stock, p_page, f_size, this.id)
+      .subscribe(
+        response => {
+          this.result = response;
+          if (this.result.success) {
+            this.laminations = this.result.data;
+          } else {
+            this.laminations = [{ id: 0, name: 'No Record Found' }];
+          }
+        }
+      )
+  }
+
+  change_lamination(p_lamination: any, p_cover: any, p_stock: any, p_page: any, f_size: any) {
+    this.prices = '';
+    this.productService.getProductprices(p_lamination, p_cover, p_stock, p_page, f_size, this.id)
+      .subscribe(
+        response => {
+          this.result = response;
+          if (this.result.success) {
+            this.prices = this.result.data;
+          } else {
+            this.prices = [{ id: 0, name: 'No Record Found' }];
+          }
+        }
+      )
+  }
+
+
 }
